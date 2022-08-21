037812cff23ca53cf6956fdf6b7c42e09a6c85b7
PC-ACER <PC-ACER@UTKARSH>
2022-08-22 01:36:18 +0530

    Backend Adminfor Learners Academy

---
+++ admin/src/main/java/com/operations/CreateSubject.java
@@ -0,0 +1,63 @@
+package com.operations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.AllDetails.StudentDetails;
+import com.AllDetails.SubjectDetails;
+import com.util.Util;
+
+@WebServlet("/subject")
+public class CreateSubject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		request.getRequestDispatcher("add-subject.html").include(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("Name");
+		String code = request.getParameter("code");
+		String teacher = request.getParameter("teacher");
+		
+		try {
+			SessionFactory factory = Util.buildConnection();
+			
+			Session session = factory.openSession();
+			
+			SubjectDetails subjectdetails = new SubjectDetails(name, code, teacher);
+			
+			Transaction tx = session.beginTransaction();
+			
+			session.save(subjectdetails);
+			
+			tx.commit();
+			
+			if(session != null) {
+				out.println("<div align='center'><h2 style='color:green'>SUBJECT ADDED SUCCESSFULLY!</h2>"
+						+ "<a href='admin-page.html'>RETURN To MainMenu</a>"
+						+ "</div>");
+			}
+			session.close();
+		} catch (Exception e) {
+			out.println("<div align='center'><h2 style='color:red'>TRY AGAIN! OPERATION FAILED...</h2>"
+					+ "<a href='admin-page.html'>RETURN To MainMenu</a>"
+					+ "</div>");
+		}
+	}
+
+}
---
+++ admin/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<html>
+<title>LEARNER'S ACADEMY</title>
+<body>
+<div align="center">
+		<h2>WELCOME TO LEARNER'S ACADEMY</h2>
+		
+		<a href="login">CLICK TO LOGIN TO ADMIN PORTAL</a><br><br><br><br><br><br><br>
+		<h3>DEVELOPER NAME :UTKARSH DHURANDHAR</h3>
+</div>
+</body>
+<footer>
+<div align="center">
+<br><br><br><br><br><br><br><br><br><br><br>
+<a href="init">DATABASE STATUS</a><br>
+</div>
+</footer>
+</html>
